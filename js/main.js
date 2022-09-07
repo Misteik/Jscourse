f200355d664d63905e54e83db8f1178d3f8a5d60
Slava <misteik.pr@gmail.com>
2022-09-07 19:56:26 +0300

    gg

---
+++ js/main.js
@@ -0,0 +1,38 @@
+"use strict"
+
+// const result = confirm('Are you here?');
+// console.log(result);
+
+// const answer =  prompt('Вам есть 18?','18');
+// console.log(answer + 5);
+
+// const answers = [];
+//
+// // answers[0] = prompt('Как ваше имя?','');
+// // answers[1] = prompt('Как ваши дела?','');
+// // answers[2] = prompt('Какое у вас отчество?','');
+//
+// console.log(typeof (answers));
+
+
+//Интерполяция
+// const category = 'toys';
+//
+// console.log(`https://someurl.com/${category}`);
+//
+// const user = 'Ivan';
+//
+// alert(`Привет, ${user}`);
+
+//Инкременты, декременты
+// let incr = 10,
+//     decr = 10;
+//
+// // ++incr;
+// // --decr;
+// console.log(++incr);
+// console.log(--decr);
+//
+// const isChecked = true,
+//     isClose = false;
+// console.log(isChecked || isClose);
